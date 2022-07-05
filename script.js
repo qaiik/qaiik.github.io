2a20ab71aaf23102a72ade0471118b37285069dc
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-07-04 23:20:04 -0400

    v1.0

---
+++ script.js
@@ -0,0 +1,30 @@
+function blookforcebtn() {
+  alert("Blookforce:\nMade by me and acai (or l2vy7)\n\nGets the PINs of every current Blooket game.\n\nStats: updated");
+}
+
+function blktreaderbtn() {
+  alert("blkt-reader:\n\nA ton of Blooket Hacks.\n\nStats: semi-updated");
+  window.open('https://github.com/qaiik/blkt-reader', 'popUpWindow','height=600,width=1000,left=5,top=5');
+}
+
+function gameruinbtn() {
+  alert("Blooket Game Ruiner:\nMade by me and zastix (or notzasitx)\n\nA few Blooket Hacks.\n\nStats: semi-updated");
+  window.open('https://github.com/notzastix/blooketGameRuiner', 'popUpWindow','height=600,width=1000,left=5,top=5');
+}
+
+function viscountbtn() {
+  alert("README visitor count:\n\nA visitor count for READMEs.\n\nStats: not updated");
+  window.open('https://github.com/qaiik/readmevisitorcount', 'popUpWindow','height=600,width=1000,left=5,top=5');
+}
+
+function github() {
+  window.open('https://github.com/qaiik', 'popUpWindow','height=600,width=1000,left=5,top=5');
+}
+
+function replit() {
+  window.open('https://replit.com/@qaiik', 'popUpWindow','height=600,width=1000,left=5,top=5');
+}
+
+function discord() {
+  alert("24thdev#6256");
+}
